72b99083d2695cbe55a131fb3ba213699d779fc6
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-02-19 22:19:59 +0200

    renderDone

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('.form');
 const listImages = document.querySelector('ul.gallery');
 
-
 form.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(event) {
@@ -17,8 +16,10 @@ function handleFormSubmit(event) {
     return;
   }
   console.log('searchText');
+ 
+  listImages.innerHTML = '';
 
-   getPhotosByText(searchText)
+  getPhotosByText(searchText)
     .then(data => {
       if (data.hits.length === 0) {
         iziToast.error({
@@ -31,12 +32,12 @@ function handleFormSubmit(event) {
       }
       console.log(data.hits);
       template(data.hits);
-     
-      var lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
 
+      var lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      lightbox.refresh();
     })
     .catch(error => {
       console.error(error);
@@ -46,7 +47,7 @@ function handleFormSubmit(event) {
   //   body: { API_KEY: "42434197-39cd5bed035ab257afa598fe1", q: "roses" },
   // };
   // fetch('https://pixabay.com/api/', options)
-   form.reset();
+  form.reset();
 }
 
 function getPhotosByText(searchText) {
@@ -70,8 +71,9 @@ function getPhotosByText(searchText) {
 }
 
 function template(obj) {
-    const markup = obj.map(
-    image => `<li class='gallery-item'>
+  const markup = obj
+    .map(
+      image => `<li class='gallery-item'>
   <a class='gallery-link' href='${image.largeImageURL}'>
     <img
       class='gallery-image'
@@ -79,11 +81,41 @@ function template(obj) {
       alt='${image.tags}'
     />
   </a>
+    <ul class="gallery-item-info">
+    <li class='gallery-item-info-categories'> 
+            <div class='gallery-item-info-likes'>
+                    <h3 class="gallery-item-info-title">Likes</h3>
+                <p class="gallery-item-info-text">${image.likes}</p>
+                </div>
+    </li>
+    <li class='gallery-item-info-categories'> 
+            <div class='gallery-item-info-views'>
+                    <h3 class="gallery-item-info-title">Views</h3>
+                <p class="gallery-item-info-text">${image.views}</p>
+                  </div>
+    </li>
+    <li class='gallery-item-info-categories'> 
+            <div class='gallery-item-info-comments'>
+                    <h3 class="gallery-item-info-title">Comments</h3>
+                <p class="gallery-item-info-text">${image.comments}</p>
+                 </div>
+    </li>
+    <li class='gallery-item-info-categories'> 
+            <div class='gallery-item-info-downloads'>
+                    <h3 class="gallery-item-info-title">Downloads</h3>
+                <p class="gallery-item-info-text">${image.downloads}</p>
+                 </div>
+    </li>
+    </ul>  
 </li>`
-  )
+    )
     .join('');
-  
+
   listImages.insertAdjacentHTML('afterbegin', markup);
 
   // return ``
 }
+
+// function render(obj) {
+  
+// }
